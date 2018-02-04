6c8ea1280e91098df38bbdcc69394515e1dead4f
clivejj <cjohnston1@stuy.edu>
2018-02-04 12:29:52 -0500

    done:

--- script.js
+++ script.js
@@ -9,9 +9,38 @@ var toggleOn = 1;
 var ctx = canvas.getContext("2d");
 
 toggleButton.addEventListener('click', toggle);
-var toggle = function(e) {
+
+function toggle(e) {
     toggleOn *= -1;
     console.log(toggleOn);
 };
+
+clearButton.addEventListener('click', clear);
+
+function clear(e) {
+    console.log("clearing!");
+    ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+};
+
+canvas.addEventListener('click', draw);
+function draw(e) {
+    console.log("drawing!");
+    var rect = canvas.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    if (toggleOn == -1) {
+	ctx.beginPath();
+	ctx.arc(x, y, 20, 0, 2 * Math.PI);
+	ctx.fill();
+    };
+    if (toggleOn == 1) {
+	ctx.beginPath();
+	ctx.rect(x - 20, y - 20, 40, 40);
+	ctx.fill();
+    };
+};
+    
+
+
 
     
